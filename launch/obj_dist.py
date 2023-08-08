6d09fbf70f588fefe030a0d125d7e1a42a149942
HopeCollector <cmw4187@126.com>
2023-08-08 10:10:53 +0800

    chore<launch/obj_dist>: 新增 launch 文件用于启动 obj_dist

---
+++ launch/obj_dist.py
@@ -0,0 +1,25 @@
+import launch
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+
+
+def generate_launch_description():
+    """Generate launch description with multiple components."""
+    container = ComposableNodeContainer(
+            name='obj_dist_container',
+            namespace='',
+            package='rclcpp_components',
+            executable='component_container',
+            composable_node_descriptions=[
+                ComposableNode(
+                    package='laser_copilot',
+                    plugin="laser_copilot::obj_dist",
+                    name='objdist',
+                    remappings=[
+                        ('sub/lvx', '/livox/lidar')
+                    ])
+            ],
+            output='screen',
+    )
+
+    return launch.LaunchDescription([container])
