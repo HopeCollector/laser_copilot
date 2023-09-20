7af3b8b79b57b4123cca51d2687c1d04d2a651db
HopeCollector <cmw4187@126.com>
2023-09-20 02:52:05 +0000

    fix<obj_dist>: 修复没有方法加载参数的问题

--- launch/obj_dist.py
+++ launch/obj_dist.py
@@ -1,25 +1,36 @@
+import sys
 import launch
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
+from launch_ros.substitutions import FindPackageShare, PathJoinSubstitution
+
+
+def get_cfg_file():
+    return [
+        PathJoinSubstitution([FindPackageShare("laser_copilot"), "launch", "obj_dist.yaml"])
+    ]
 
 
 def generate_launch_description():
     """Generate launch description with multiple components."""
     container = ComposableNodeContainer(
-            name='laser_copilot_container',
-            namespace='',
-            package='rclcpp_components',
-            executable='component_container',
-            composable_node_descriptions=[
-                ComposableNode(
-                    package='laser_copilot',
-                    plugin="laser_copilot::obj_dist",
-                    name='objdist',
-                    remappings=[
-                        ('sub/lvx', '/livox/lidar')
-                    ])
-            ],
-            output='screen',
+        name="laser_copilot_container",
+        namespace="",
+        package="rclcpp_components",
+        executable="component_container",
+        composable_node_descriptions=[
+            ComposableNode(
+                package="laser_copilot",
+                plugin="laser_copilot::obj_dist",
+                name="objdist",
+                parameters=get_cfg_file(),
+                remappings=[
+                    ("sub/lvx", "livox/lidar"),
+                    ("out/laser_scan", "mavros/obstacle/send"),
+                ],
+            )
+        ],
+        output="screen",
     )
 
     return launch.LaunchDescription([container])
