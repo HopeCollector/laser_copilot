67f1231b8dfe0ece289b55b4da6cabfb403f7125
HopeCollector <cmw4187@126.com>
2023-12-06 01:06:07 +0000

    perf<visualization_helper>: 提升 guisetpoint 的可用性

--- laser_copilot_applications/src/visualization_helper.cc
+++ laser_copilot_applications/src/visualization_helper.cc
@@ -1,5 +1,8 @@
 #include <rclcpp/rclcpp.hpp>
 #include <tf2_ros/static_transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <std_msgs/msg/float64_multi_array.hpp>
 #include <geometry_msgs/msg/pose_stamped.hpp>
 #include <visualization_msgs/msg/marker.hpp>
@@ -7,6 +10,9 @@
 #include "common.hh"
 
 namespace laser_copilot_applications {
+
+constexpr double ARROW_MAX_LEN = 1.0;
+
 class visualization_helper : public rclcpp::Node {
 public:
   explicit visualization_helper(const rclcpp::NodeOptions &options)
@@ -31,14 +37,15 @@ public:
         "pub/setpoint", 10);
     pub_velocity_ = create_publisher<visualization_msgs::msg::Marker>(
         "pub/velocity", 10);
-
-    msg_path_.header.frame_id = "odom";
+    tf_buffer_ = std::make_shared<tf2_ros::Buffer>(get_clock());
+    tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+    odom_frame_id_ = declare_parameter("odom_frame_id", "odom");
   }
 
 private:
   void cb_controller_info(const std_msgs::msg::Float64MultiArray::SharedPtr msg) {
     visualization_msgs::msg::Marker marker;
-    marker.header.frame_id = "odom";
+    marker.header.frame_id = odom_frame_id_;
     marker.header.stamp = get_clock()->now();
     marker.ns = "laser_copilot";
     marker.id = 0;
@@ -47,44 +54,56 @@ private:
     marker.points.resize(2);
     auto &start_point = marker.points[0];
     auto &end_point = marker.points[1];
-    double speed = msg->data[3];
+    double speed = msg->data[8];
+    double scale = 1.0 / speed * std::min(speed, ARROW_MAX_LEN);
     start_point.x = msg->data[0];
     start_point.y = msg->data[1];
     start_point.z = msg->data[2];
-    end_point.x = start_point.x + msg->data[8] * speed;
-    end_point.y = start_point.y + msg->data[9] * speed;
-    end_point.z = start_point.z + msg->data[10] * speed;
-    marker.scale.x = 0.1 * speed;
-    marker.scale.y = 0.2 * speed;
-    marker.scale.z = 0.2 * speed;
-    marker.color.a = 1.0;
-    marker.color.r = 1.0;
-    marker.color.g = 1.0;
-    marker.color.b = 0.0;
+    end_point.x = start_point.x + msg->data[9] * scale;
+    end_point.y = start_point.y + msg->data[10] * scale;
+    end_point.z = start_point.z + msg->data[11] * scale;
+    marker.scale.x = 0.1 * scale;
+    marker.scale.y = 0.2 * scale;
+    marker.scale.z = 0.2 * scale;
     pub_velocity_->publish(marker);
 
     cur_pos_.position << msg->data[0], msg->data[1], msg->data[2];
-    geometry_msgs::msg::PoseStamped msg_pose;
     msg_path_.header.stamp = get_clock()->now();
-    msg_pose.header = msg_path_.header;
-    msg_pose.pose.position.x = cur_pos_.position.x();
-    msg_pose.pose.position.y = cur_pos_.position.y();
-    msg_pose.pose.position.z = cur_pos_.position.z();
-    msg_pose.pose.orientation.w = 1.0;
-    msg_pose.pose.orientation.x = 0.0;
-    msg_pose.pose.orientation.y = 0.0;
-    msg_pose.pose.orientation.z = 0.0;
-    msg_path_.poses.push_back(msg_pose);
+    msg_path_.header.frame_id = odom_frame_id_;
+    if (speed > 0.05) {
+      geometry_msgs::msg::PoseStamped msg_pose;
+      msg_pose.header = msg_path_.header;
+      msg_pose.pose.position.x = cur_pos_.position.x();
+      msg_pose.pose.position.y = cur_pos_.position.y();
+      msg_pose.pose.position.z = cur_pos_.position.z();
+      msg_pose.pose.orientation.w = 1.0;
+      msg_pose.pose.orientation.x = 0.0;
+      msg_pose.pose.orientation.y = 0.0;
+      msg_pose.pose.orientation.z = 0.0;
+      msg_path_.poses.push_back(msg_pose);
+    }
     pub_path_real_->publish(msg_path_);
   }
 
   void cb_gui_setpoint(const geometry_msgs::msg::PoseStamped::SharedPtr msg) {
-    auto new_msg = *msg;
-    new_msg.pose.position.z = cur_pos_.position.z();
-    pub_setpoint_->publish(new_msg);
+    static rclcpp::Rate rate(1);
+    static geometry_msgs::msg::PoseStamped new_msg;
+    do {
+      try {
+        auto tf = tf_buffer_->lookupTransform(odom_frame_id_, msg->header.frame_id,
+                                    tf2::TimePointZero);
+        tf2::doTransform(*msg, new_msg, tf);
+        pub_setpoint_->publish(new_msg);
+        break;
+      } catch (tf2::TransformException &ex) {
+        RCLCPP_WARN_STREAM(get_logger(), "waiting for transform from "
+                                             << msg->header.frame_id << " to "
+                                             << odom_frame_id_);
+      }
+    } while (rate.sleep());
 
     nav_msgs::msg::Path msg_path_stright;
-    msg_path_stright.header.frame_id = "odom";
+    msg_path_stright.header.frame_id = odom_frame_id_;
     msg_path_stright.header.stamp = get_clock()->now();
     msg_path_stright.poses.resize(2);
     auto &start_point = msg_path_stright.poses[0];
@@ -133,8 +152,11 @@ private:
   rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr pub_path_stright_;
   rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr pub_path_real_;
   rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr pub_velocity_;
+  std::shared_ptr<tf2_ros::TransformListener> tf_listener_ = nullptr;
+  std::shared_ptr<tf2_ros::Buffer> tf_buffer_ = nullptr;
   pose_t cur_pos_;
   nav_msgs::msg::Path msg_path_;
+  std::string odom_frame_id_;
 };
 }; // namespace laser_copilot_applications
 
