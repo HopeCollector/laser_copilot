9fd479828c11b8a987fe973665801ab3496843db
HopeCollector <cmw4187@126.com>
2023-12-18 08:37:33 +0000

    chore<obj_dist launch>: change sub/lvx remapping

--- laser_copilot_applications/launch/obj_dist.launch.py
+++ laser_copilot_applications/launch/obj_dist.launch.py
@@ -28,7 +28,7 @@ def get_composable_node():
             }
         ],
         remappings=[
-            ("sub/lvx", "livox/lidar"),
+            ("sub/lvx", "/livox/lidar"),
             ("sub/pc2", "/x500_lidar/point_cloud"),
             ("out/laser_scan", "laser_scan/objs"),
         ],
